327b864e39452e0e3f0e9dbf398baea2af0596b5
Trey Jackson <jacksrf@gmail.com>
2014-12-14 10:08:34 -0500

    added img, links, and remove/add online list as well as logout button

--- new_client_browser.js
+++ new_client_browser.js
@@ -1,27 +1,18 @@
 var client = new WebSocket("ws://localhost:3000");
 
 var reminderCount = 0;
-
 var userName = "";
+var userNames = [];
 
 client.addEventListener("open", function(connection) {
-  console.log('connected');
-
-  connection.onopen = function() {
-    client.send(userName);
-  }
-
   //HTML elements
   var body = document.querySelector("body");
-  var ul = document.querySelector("ul");
-  var button = document.querySelector("button");
+  var chatMessages = document.getElementById("chatMessages");
+  var loggedList = document.getElementById("loggedList");
+  var submit = document.getElementById("submit");
+  var logout = document.getElementById("logout");
 
-  // var li = document.createElement("li");
-  // li.style.color = userColor;
-  // li.innerText = "Hi " + userName + "! Remember your color is " + userColor;
-  // ul.insertBefore(li, ul.firstChild);
-
-  button.addEventListener("click", function() {
+  submit.addEventListener("click", function() {
     var yourSent = document.getElementById("textBox");
 
     var nameInput = document.getElementById("userNameInput");
@@ -32,24 +23,28 @@ client.addEventListener("open", function(connection) {
     var colorChoice = document.getElementById("ddl");
     var userColor = colorChoice.options[colorChoice.selectedIndex].value;
     if (nameInput.value.trim() !== "" && userColor.trim() !== "") {
-      
+
       if (reminderCount === 0) {
         var li = document.createElement("li");
         li.style.color = userColor;
         li.innerText = "Hi " + userName + "! Remember your color is " + userColor;
-        ul.insertBefore(li, ul.firstChild);
+        chatMessages.insertBefore(li, chatMessages.firstChild);
         reminderCount++;
       }
 
 
     //create messageObject with name and message
-    var messageObject = {name: userName, msg: yourSent.value, color: userColor};
+    var messageObject = {name: userName, msg: yourSent.value, color: userColor, loggedIn: userName};
+
     //take messageObject, stringify and send to server
 
     //will only send something if the input actually has text
     if (yourSent.value.trim() != "") {
       client.send(JSON.stringify(messageObject));
     }
+
+
+
 
 
     //resets input box
@@ -65,7 +60,7 @@ client.addEventListener("open", function(connection) {
 
   input.addEventListener("keypress", function(){
     if(event.keyCode === 13){
-      button.click();
+      submit.click();
     }
   });
 
@@ -73,24 +68,95 @@ client.addEventListener("open", function(connection) {
   client.addEventListener("message", function(message) {
     //recieves message from server and parses the data
     var newMessage = JSON.parse(message.data);
-    console.log(newMessage);
-    var ul = document.querySelector("ul");
-    var li = document.createElement("li");
 
-    // if (newMessage.length === 1) {
+    console.log(userNames);
+    console.log(newMessage.remove);
+
+    userNames.forEach(function(user) {
+      if (user === newMessage.remove) {
+        console.log(newMessage.remove)
+        var loggedList = document.getElementById("loggedList");
+        var userRemove = document.getElementById(newMessage.remove);
+        loggedList.removeChild(userRemove);
+      }
+    })
+
+
+    if (userNames.length > 0) {
+    var index = userNames.indexOf(newMessage.loggedIn);
+      if (index === -1 && newMessage.name !== "ADMIN") {
+      var chatMessages = document.getElementById("chatMessages");
+      var li = document.createElement("li");
+
+      li.style.color = "black";
+      li.innerText = newMessage.name + ": has logged into the chat";
+      chatMessages.insertBefore(li, chatMessages.firstChild);
+    }
+  }
+
+    var index2 = userNames.indexOf(newMessage.loggedIn);
+      if (index2 === -1 && newMessage.loggedIn !== undefined) {
+        var loggedList = document.getElementById("loggedList");
+        var li = document.createElement("li");
+
+      li.style.color = "green";
+      li.innerHTML = newMessage.loggedIn;
+      li.setAttribute("id", newMessage.loggedIn);
+      loggedList.appendChild(li);
+    }
+
+
+
+    var chatMessages = document.getElementById("chatMessages");
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    var img = document.createElement("img");
+    var letterCheck = newMessage.msg;
+    var length = newMessage.msg.length;
+
+   if (letterCheck.charAt(length-2)+letterCheck.charAt(length-1)+letterCheck.charAt(length) === "jpg" || letterCheck.charAt(length-2)+letterCheck.charAt(length-1)+letterCheck.charAt(length) === "png" ) {
+      li.style.color = newMessage.color;
+      a.setAttribute("href", newMessage.msg);
+      a.innerText = newMessage.name + ": " + newMessage.msg;
+      li.appendChild(a);
+      chatMessages.insertBefore(li, chatMessages.firstChild);
+    } else if (letterCheck.charAt(length-2)+letterCheck.charAt(length-1)+letterCheck.charAt(length) === "bmp" || letterCheck.charAt(length-2)+letterCheck.charAt(length-1)+letterCheck.charAt(length) === "gif" ) {
+      li.style.color = newMessage.color;
+      a.setAttribute("href", newMessage.msg);
+      a.innerText = newMessage.name + ": " + newMessage.msg;
+      li.appendChild(a);
+      chatMessages.insertBefore(li, chatMessages.firstChild);
+    } else if (letterCheck.charAt(0) === "h" && letterCheck.charAt(1) ==="t" && letterCheck.charAt(2) ==="t" && letterCheck.charAt(3) ==="p" ) {
+      li.style.color = newMessage.color;
+      img.setAttribute("height","200px")
+      img.setAttribute("src", newMessage.msg);
+      li.innerHTML = newMessage.name + ": ";
+      li.appendChild(img);
+      chatMessages.insertBefore(li, chatMessages.firstChild);
+    } else {
       li.style.color = newMessage.color;
       li.innerText = newMessage.name + ": " + newMessage.msg;
-      //this will put the list element at the top of the list
-      ul.insertBefore(li, ul.firstChild);
-    // } else {
-    //   for (i=0; i<newMessage.length; i++) {
-    //     li.style.color = newMessage[i].color;
-    //     li.innerText = newMessage[i].name + ": " + newMessage[i].msg;
-    //     //this will put the list element at the top of the list
-    //     ul.insertBefore(li, ul.firstChild);
-    //   }
-    //  }
+      chatMessages.insertBefore(li, chatMessages.firstChild);
+    }
+
+userNames.push(newMessage.name);
   });
 
+  logout.addEventListener("click", function() {
+    var nameInput = document.getElementById("userNameInput");
+    var name = nameInput.value;
 
-});//end connection event
+    var userName = name.toUpperCase();
+
+        var finalMessage = {name: "ADMIN", msg: "a user has left the chat", color: "black", remove: userName};
+        client.send(JSON.stringify(finalMessage));
+    })
+});
+
+function close_window() {
+  if (confirm("Close Window?")) {
+    close();
+  }
+}
+
+//end connection event
--- server_node.js
+++ server_node.js
@@ -3,25 +3,13 @@ var server = new WSS({port: 3000});
 
 var clients = [];
 var history = [];
+var userNames = [];
 
 server.on("connection", function(connected) {
 
-  clients.forEach(function(client) {
-    var newPerson = {name: "user[]", msg: "has joined the chat", color: "black"}
-    client.send(JSON.stringify(newPerson));
-  })
-
   clients.push(connected);
   console.log(clients.length);
-  // if (clients.length === 1){
-  //   var welcomeOne = {msg : "You are the only one connected!"}
-  //   connected.send(JSON.stringify(welcomeOne));
-  // } else {
-  // clients.forEach(function(client) {
-  //   var welcomeOthers = {msg: "someone else just connected -- " + clients.length + " people connected" }
-  //   client.send(JSON.stringify(welcomeOthers));
-  //   });
-  // }
+
   if (history.length > 0) {
     history.forEach(function(msg){
       connected.send(msg);
@@ -29,25 +17,10 @@ server.on("connection", function(connected) {
   }
 
   connected.on("message", function(message) {
-    // var messageObject = JSON.parse(message)
-    // console.log(messageObject);
-    // var serverMessage = (messageObject.name + ": " + messageObject.msg);
-    // console.log(serverMessage);
-
 
     history.push(message);
-    // var tempClients = [];
     clients.forEach(function(client) {
         client.send(message);
-    // })
-    // var x = tempClients.indexOf(connected)
-    // tempClients.splice(x, 1);
-    // tempClients.forEach(function(client) {
-      // if (msg == "table flip") {
-      //   client.send("(╯°□°）╯︵ ┻━┻");
-      // } else if (msg == "sniper rifle"){
-      //   client.send("︻デ┳═ー");
-      // } else {
       })
   });
 
